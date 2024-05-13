dde540887d13c4005e25b13cc84cce2914bb3e5a
UserBens <benedictusradyan@gmail.com>
2024-05-13 11:57:18 +0700

    dev cronjob pastdue charge

--- app/Http/Controllers/Notification/NotificationPastDue.php
+++ app/Http/Controllers/Notification/NotificationPastDue.php
@@ -23,140 +23,151 @@ use PDO;
 
 class NotificationPastDue extends Controller
 {
-    public function cronChargePastDue($type = 'SPP', $charge = false)
-    {
+   public function cronChargePastDue($type = 'SPP', $charge = false)
+   {
       DB::beginTransaction();
-        try {
+      try {
 
-           date_default_timezone_set('Asia/Jakarta');
-  
-           if($charge) 
-           {
-                $billCharge = Bill::with('bill_installments')->where('paidOf', false)->where('deadline_invoice', '<', date('Y-m-d'))->where('type', $type)->get(['id', 'amount', 'charge', 'installment', 'amount_installment']);
-                foreach ($billCharge as $bill) {
-                   # code...
-                   Bill::where('id', $bill->id)->update([
-                      'amount'=> $bill->amount + 100_000,
-                      'charge'=> $bill->charge + 100_000,
-                      'amount_installment' => $bill->installment? $bill->amount_installment + 100_000 : $bill->amount_installment,
+         date_default_timezone_set('Asia/Jakarta');
+
+         // if ($charge) {
+         //    $billCharge = Bill::with('bill_installments')->where('paidOf', false)->where('deadline_invoice', '<', date('Y-m-d'))->where('type', $type)->get(['id', 'amount', 'charge', 'installment', 'amount_installment']);
+         //    foreach ($billCharge as $bill) {
+         //       # code...
+         //       Bill::where('id', $bill->id)->update([
+         //          'amount' => $bill->amount + 100_000,
+         //          'charge' => $bill->charge + 100_000,
+         //          'amount_installment' => $bill->installment ? $bill->amount_installment + 100_000 : $bill->amount_installment,
+         //       ]);
+
+         //       foreach ($bill->bill_installments as $installment) {
+
+         //          if ($installment->pivot->main_id != $installment->pivot->child_id) {
+
+         //             Bill::where('id', $installment->pivot->child_id)->update([
+         //                'amount' => $bill->amount + 100_000,
+         //             ]);
+         //          }
+         //       }
+         //    }
+         // }
+
+         if ($charge) {
+            $billCharge = Bill::with('bill_installments')->where('paidOf', false)->where('deadline_invoice', '<', date('Y-m-d'))->where('type', $type)->get(['id', 'amount', 'charge', 'installment', 'amount_installment']);
+            foreach ($billCharge as $bill) {
+               # code...
+               Bill::where('id', $bill->id)->update([
+                  'charge' => 100000, // Set charge menjadi 100.000
+               ]);
+
+               foreach ($bill->bill_installments as $installment) {
+                  if ($installment->pivot->main_id != $installment->pivot->child_id) {
+                     Bill::where('id', $installment->pivot->child_id)->update([
+                        'charge' => 100000, // Set charge pada tagihan anak menjadi 100.000
                      ]);
-                     
-                  foreach ($bill->bill_installments as $installment){
-                        
-                     if($installment->pivot->main_id != $installment->pivot->child_id) {
+                  }
+               }
+            }
+         }
 
-                        Bill::where('id', $installment->pivot->child_id)->update([
-                           'amount'=> $bill->amount + 100_000,
-                        ]);
-                     }
+         if ($type == 'etc') {
 
-                   }
-                }
-           }
-
-         
-         if($type == 'etc') 
-         {
-
-            $data = Student::with(['bill' => function($query) use ($type){
+            $data = Student::with(['bill' => function ($query) use ($type) {
                $query
-               ->whereNotIn('type', ['SPP', "Capital Fee", "Paker", "Book", "uniform"])
-               ->where('deadline_invoice', '<', date('Y-m-d'))
-               ->where('paidOf', false)
-               ->get();
-            }, 'relationship'])->whereHas('bill', function($query) use ($type) {
+                  ->whereNotIn('type', ['SPP', "Capital Fee", "Paker", "Book", "uniform"])
+                  ->where('deadline_invoice', '<', date('Y-m-d'))
+                  ->where('paidOf', false)
+                  ->get();
+            }, 'relationship'])->whereHas('bill', function ($query) use ($type) {
                $query
-               ->whereNotIn('type', ['SPP', "Capital Fee", "Paker", "Book", "uniform"])
-               ->where('paidOf', false)
-               ->where('deadline_invoice', '<', date('Y-m-d'));
+                  ->whereNotIn('type', ['SPP', "Capital Fee", "Paker", "Book", "uniform"])
+                  ->where('paidOf', false)
+                  ->where('deadline_invoice', '<', date('Y-m-d'));
             })->get();
-
          } else {
-            
-            $data = Student::with(['bill' => function($query) use ($type){
+
+            $data = Student::with(['bill' => function ($query) use ($type) {
                $query
-               ->where('type', $type)
-               ->where('deadline_invoice', '<', date('Y-m-d'))
-               ->where('paidOf', false)
-               ->get();
-            }, 'relationship'])->whereHas('bill', function($query) use ($type) {
+                  ->where('type', $type)
+                  ->where('deadline_invoice', '<', date('Y-m-d'))
+                  ->where('paidOf', false)
+                  ->get();
+            }, 'relationship'])->whereHas('bill', function ($query) use ($type) {
                $query
-               ->where('type', $type)
-               ->where('paidOf', false)
-               ->where('deadline_invoice', '<', date('Y-m-d'));
+                  ->where('type', $type)
+                  ->where('paidOf', false)
+                  ->where('deadline_invoice', '<', date('Y-m-d'));
             })->get();
          }
 
-
-               
          info($data);
-  
-           foreach ($data as $student) {
-  
-              foreach ($student->bill as $bill) {
-                 # code...
-                 $mailData = [
-                    'student' => $student,
-                    'bill' => strtolower($type) == 'book'? $bill : [$bill],
-                    'past_due' => true,
-                    'charge' => $charge,
-                    'change' => false,
-                    'is_paid' => false,
-                 ];
-  
-                 try {
-                    //code...
 
-                    $bill_type = $bill->type;
+         foreach ($data as $student) {
 
-                    if($bill->type == "SPP") {
+            foreach ($student->bill as $bill) {
+               # code...
+               $mailData = [
+                  'student' => $student,
+                  'bill' => strtolower($type) == 'book' ? $bill : [$bill],
+                  'past_due' => true,
+                  'charge' => $charge,
+                  'change' => false,
+                  'is_paid' => false,
+               ];
+
+               try {
+                  //code...
+
+                  $bill_type = $bill->type;
+
+                  if ($bill->type == "SPP") {
                      $bill_type = "Monthly Fee";
-                    } else if($bill->type == "Book") {
+                  } else if ($bill->type == "Book") {
                      $bill_type = "Material Fee";
-                    }
+                  }
 
-                     $subs = $charge? "Pemberitahuan Tagihan " . $bill->type . " terkena charge karena sudah melewati jatuh tempo" : "Tagihan " . $bill->type . " " . $student->name ." sudah melewati jatuh tempo";
+                  $subs = $charge ? "Pemberitahuan Tagihan " . $bill->type . " terkena charge karena sudah melewati jatuh tempo" : "Tagihan " . $bill->type . " " . $student->name . " sudah melewati jatuh tempo";
 
-                     $array_email = [];
+                  $array_email = [];
 
-                    foreach ($student->relationship as $key => $parent) {
-                        
-                     if($key == 0) $mailData['name'] = $parent->name;
-                     
+                  foreach ($student->relationship as $key => $parent) {
+
+                     if ($key == 0) $mailData['name'] = $parent->name;
+
                      array_push($array_email, $parent->email);
-                        //   return view('emails.spp-mail')->with('mailData', $mailData);
-                           
-                        //   return view('emails.fee-regis-mail')->with('mailData', $mailData);
-                        
-                        //   return view('emails.paket-mail')->with('mailData', $mailData);
+                     //   return view('emails.spp-mail')->with('mailData', $mailData);
 
-                        // return view('emails.book-mail')->with('mailData', $mailData);
-                     }
+                     //   return view('emails.fee-regis-mail')->with('mailData', $mailData);
 
-                     dispatch(new SendEmailJob($array_email, strtolower($type), $mailData, $subs, $bill->id));
-  
-  
-                 } catch (Exception) {
-  
-                    statusInvoiceMail::create([
-                       'bill_id' => $bill->id,
-                       'status' => false,
-                       'charge' => $charge,
-                       'past_due' => true,
-                    ]);
-                 }
-              }
-  
-              
-           }
+                     //   return view('emails.paket-mail')->with('mailData', $mailData);
 
-           DB::commit();
+                     // return view('emails.book-mail')->with('mailData', $mailData);
 
-           info("Cron Job past due success at ". date('d-m-Y'));
-           
-       } catch (Exception $err) {
-          DB::rollBack();
-          info("Cron Job pastdue Error at: " . $err);
-       }
-    }
+                     $pdf = app('dompdf.wrapper');
+                     // Perhatikan bahwa kita menggunakan $mailData['bill'][0] untuk setiap siswa
+                     $pdf->loadView('components.bill.pdf.paid-pdf', ['data' => $mailData['bill'][0]])->setPaper('a4', 'portrait');
+                     Mail::to($parent->email)->send(new SppMail($mailData, "Tagihan SPP " . $mailData['student']->name .  " bulan ini, " . date('F Y') . " sudah dibuat.", $pdf));
+                  }
+
+                  dispatch(new SendEmailJob($array_email, strtolower($type), $mailData, $subs, $bill->id));
+               } catch (Exception) {
+
+                  statusInvoiceMail::create([
+                     'bill_id' => $bill->id,
+                     'status' => false,
+                     'charge' => $charge,
+                     'past_due' => true,
+                  ]);
+               }
+            }
+         }
+
+         DB::commit();
+
+         info("Cron Job past due success at " . date('d-m-Y'));
+      } catch (Exception $err) {
+         DB::rollBack();
+         info("Cron Job pastdue Error at: " . $err);
+      }
+   }
 }
